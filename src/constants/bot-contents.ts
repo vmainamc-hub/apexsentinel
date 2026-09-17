f7fa72907f1e35fa3e218c0e84d4382e5db5e5dd
vmainamc-hub <vmainamc@gmail.com>
2026-09-17 13:09:09 +0300

    Add Bot Store as a first-class DBot tab

--- src/constants/bot-contents.ts
+++ src/constants/bot-contents.ts
@@ -14,12 +14,13 @@ export const tabs_title: TTabsTitle = Object.freeze({
 export const DBOT_TABS: TDashboardTabIndex = Object.freeze({
     DASHBOARD: 0,
     BOT_BUILDER: 1,
-    CHART: 2,
-    TUTORIAL: 3,
+    BOT_STORE: 2,
+    CHART: 3,
+    TUTORIAL: 4,
 });
 
 export const MAX_STRATEGIES = 10;
 
-export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-charts', 'id-tutorials'];
+export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-bot-store', 'id-charts', 'id-tutorials'];
 
 export const DEBOUNCE_INTERVAL_TIME = 500;
