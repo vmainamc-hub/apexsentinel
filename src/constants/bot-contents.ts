11bda5f43d5043615a3aa53a53f4844e726929d3
vmainamc-hub <vmainamc@gmail.com>
2026-09-18 22:35:20 +0300

    Fix DTrader tab index mapping

--- src/constants/bot-contents.ts
+++ src/constants/bot-contents.ts
@@ -15,12 +15,13 @@ export const DBOT_TABS: TDashboardTabIndex = Object.freeze({
     DASHBOARD: 0,
     BOT_BUILDER: 1,
     BOT_STORE: 2,
-    CHART: 3,
-    TUTORIAL: 4,
+    DTRADER: 3,
+    CHART: 4,
+    TUTORIAL: 5,
 });
 
 export const MAX_STRATEGIES = 10;
 
-export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-bot-store', 'id-charts', 'id-tutorials'];
+export const TAB_IDS = ['id-dbot-dashboard', 'id-bot-builder', 'id-bot-store', 'id-dtrader', 'id-charts', 'id-tutorials'];
 
 export const DEBOUNCE_INTERVAL_TIME = 500;
